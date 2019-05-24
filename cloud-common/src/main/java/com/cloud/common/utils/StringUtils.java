84c3955f7f8e1cb675a08cdc61b98e3ccd39eda6
yangchenglong <yangchenglong@seewintech.com>
2019-05-24 16:03:44 +0800

    添加公共jar包类

---
+++ cloud-common/src/main/java/com/cloud/common/utils/StringUtils.java
@@ -0,0 +1,10 @@
+package com.cloud.common.utils;
+
+/**
+ * @program: cloud_example
+ * @description: 字符串工具类
+ * @author: yangchenglong
+ * @create: 2019-05-24 16:03
+ */
+public class StringUtils {
+}
