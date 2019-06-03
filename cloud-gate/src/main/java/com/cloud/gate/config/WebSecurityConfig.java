1875233f71e08a1b9fee2ac73dd6c3316ccbf837
yangchenglong <yangchenglong@seewintech.com>
2019-06-03 10:27:21 +0800

    user swagger

--- cloud-gate/src/main/java/com/cloud/gate/config/WebSecurityConfig.java
+++ cloud-gate/src/main/java/com/cloud/gate/config/WebSecurityConfig.java
@@ -1,21 +1,41 @@
 package com.cloud.gate.config;
 
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * @program: cloud_example
- * @description: csrf
+ * @description:
  * @author: yangchenglong
  * @create: 2019-05-31 21:26
  */
+@Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${permit-path}")
+    private String permitPath;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        super.configure(http);
+        http.csrf().disable()
+            .formLogin();
     }
+
+    //不需要校验的路径处理
+    private void permitAll(HttpSecurity http) throws Exception {
+        if(!StringUtils.isBlank(permitPath)){
+            for(String url : permitPath.split(",")){
+                http.authorizeRequests().antMatchers(url).permitAll();
+            }
+            http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and();
+        }
+    }
+
 }
