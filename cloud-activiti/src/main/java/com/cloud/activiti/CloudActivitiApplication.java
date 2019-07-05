7103b2ef823e6c00f7ab24511f6b586a981a6b2f
yangchenglong <yangchenglong@seewintech.com>
2019-07-05 12:42:05 +0800

    activiti工作流

--- cloud-activiti/src/main/java/com/cloud/activiti/CloudActivitiApplication.java
+++ cloud-activiti/src/main/java/com/cloud/activiti/CloudActivitiApplication.java
@@ -1,21 +1,42 @@
 package com.cloud.activiti;
 
-import com.cloud.activiti.modeler.JsonpCallbackFilter;
+import com.alibaba.fastjson.JSONObject;
+import com.cloud.activiti.utils.RedisUtils;
+import com.cloud.common.entity.ResponseMessage;
+import com.cloud.common.enums.ResponseCodeEnum;
+import com.cloud.common.utils.StringUtils;
+import com.cloud.user.constant.UserConstants;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@EnableDiscoveryClient
-@SpringBootApplication(exclude = {org.activiti.spring.boot.SecurityAutoConfiguration.class})  //排除这个否则导致启动失败
-@EnableFeignClients
-@Slf4j
-@ComponentScan({"org.activiti.rest.diagram", "com.westcatr.rd"})
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 //@EnableAsync  // 启用异步任务
-public class CloudActivitiApplication {
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class, org.activiti.spring.boot.SecurityAutoConfiguration.class})  //排除这个否则导致启动失败
+@EnableFeignClients
+@EnableTransactionManagement   //启用事务管理
+@Slf4j
+@ComponentScan({"org.activiti.rest.diagram", "com.cloud.activiti"})
+public class CloudActivitiApplication implements WebMvcConfigurer {
+
+    @Value("${filter-path}")
+    private String filterPath;//未登录需拦截路径
 
     public static void main(String[] args) {
         SpringApplication.run(CloudActivitiApplication.class, args);
@@ -23,7 +44,55 @@ public class CloudActivitiApplication {
     }
 
     @Bean
-    public JsonpCallbackFilter filter(){
-        return new JsonpCallbackFilter();
+    public HandlerInterceptor getLoginInterceptor(){
+        return new LoginInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry){
+        String[] filterPaths = this.filterPath.split(",");
+        //配置拦截路径
+        registry.addInterceptor(getLoginInterceptor())
+                .addPathPatterns(filterPaths);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry){
+        registry.addResourceHandler("/rest/api/doc/**").addResourceLocations("classpath:/swagger/dist/");
+    }
+
+    //登录拦截器
+    @Component
+    class LoginInterceptor implements HandlerInterceptor {
+
+        @Resource
+        private RedisUtils redisUtils;
+
+        @Override
+        public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+            String token = UserConstants.REDIS_PC_USER_TOKEN + ":" +request.getHeader(UserConstants.PC_ACCESS_TOKEN);
+            if (StringUtils.isMobileDevice(request)) {//移动端
+                token = UserConstants.REDIS_MOBILE_USER_TOKEN + ":" +request.getHeader(UserConstants.MOBILE_ACCESS_TOKEN);
+            }
+            if (StringUtils.isNotBlank(token) && redisUtils.exists(token)) {
+                redisUtils.refreshSessionCache(token);
+                return true;
+            } else {
+                String json = JSONObject.toJSONString(new ResponseMessage<>(ResponseCodeEnum.RETURN_CODE_100000));
+                StringUtils.print(json);
+                return false;
+            }
+        }
+
+        @Override
+        public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        }
+
+        @Override
+        public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+        }
+
     }
 }
