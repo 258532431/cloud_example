7a7aadba70ddf91e294106a7236aa2248bf90f74
yangchenglong <yangchenglong@seewintech.com>
2019-07-05 17:35:15 +0800

    activiti工作流

---
+++ cloud-user/cloud-user-server/src/main/java/com/cloud/user/server/controller/LeaveController.java
@@ -0,0 +1,43 @@
+package com.cloud.user.server.controller;
+
+import com.cloud.common.entity.ResponseMessage;
+import com.cloud.common.enums.ResponseCodeEnum;
+import com.cloud.user.entity.Leave;
+import com.cloud.user.server.service.LeaveService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @program: cloud_example
+ * @description: 请假rest
+ * @author: yangchenglong
+ * @create: 2019-05-31 16:08
+ */
+@RestController
+@Transactional
+@RequestMapping("/leave")
+@Api(tags = "请假管理", description = "请假管理rest接口")
+public class LeaveController extends BaseController {
+
+    @Resource
+    private LeaveService leaveService;
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @ApiOperation(value = "根据ID获取对象", notes = "请求需要在header中传入token")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "主键Id", dataType = "Long", paramType = "path", example = "0", required = true)
+    })
+    public ResponseMessage<Leave> get(@PathVariable Long id){
+        return new ResponseMessage(ResponseCodeEnum.RETURN_CODE_100200, leaveService.selectByPrimaryKey(id));
+    }
+
+}
--- cloud-user/cloud-user-server/src/main/java/com/cloud/user/server/service/impl/LeaveServiceImpl.java
+++ cloud-user/cloud-user-server/src/main/java/com/cloud/user/server/service/impl/LeaveServiceImpl.java
@@ -1,19 +1,12 @@
 package com.cloud.user.server.service.impl;
 
-import com.cloud.common.config.GlobalException;
-import com.cloud.common.enums.ResponseCodeEnum;
 import com.cloud.common.mybatis.BaseServiceImpl;
-import com.cloud.common.utils.StringUtils;
 import com.cloud.user.entity.Leave;
-import com.cloud.user.entity.User;
 import com.cloud.user.server.mapper.LeaveMapper;
-import com.cloud.user.server.mapper.UserMapper;
 import com.cloud.user.server.service.LeaveService;
-import com.cloud.user.server.service.UserService;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.Date;
 import java.util.List;
 
 /**
