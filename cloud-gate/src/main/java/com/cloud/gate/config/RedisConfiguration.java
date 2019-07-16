6387c569cdc5a92f6fe7616564d8f8954ef02086
yangchenglong <yangchenglong@seewintech.com>
2019-07-16 14:42:13 +0800

    redis在管理工具中key乱码问题

--- cloud-gate/src/main/java/com/cloud/gate/config/RedisConfiguration.java
+++ cloud-gate/src/main/java/com/cloud/gate/config/RedisConfiguration.java
@@ -1,5 +1,8 @@
 package com.cloud.gate.config;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +10,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * @Author: yangchenglong on 2019/6/27
@@ -25,6 +30,21 @@ public class RedisConfiguration {
         try {
             RedisTemplate<Object, Object> template = new RedisTemplate<Object,Object>();
             template.setConnectionFactory(redisConnectionFactory);
+
+            //默认使用的JdkSerializationRedisSerializer 这样的会导致我们通过redis desktop manager显示的我们key跟value的时候显示不是正常字符
+            //使用Jackson2JsonRedisSerializer来序列化和反序列化redis的value值
+            Jackson2JsonRedisSerializer serializer = new Jackson2JsonRedisSerializer(Object.class);
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+            mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+            serializer.setObjectMapper(mapper);
+
+            template.setValueSerializer(serializer);
+            //使用StringRedisSerializer来序列化和反序列化redis的key值
+            template.setKeySerializer(new StringRedisSerializer());
+            template.setHashKeySerializer(new StringRedisSerializer());
+            template.setHashValueSerializer(serializer);
+            template.afterPropertiesSet();
             return template;
         } catch (Exception e){
             log.error("========> RedisConfiguration error : ", e);
